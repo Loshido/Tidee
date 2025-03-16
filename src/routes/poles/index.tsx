0a0dd5b93bff5f6d7ade413a6c7bb297e1b97eb3
loshido <loshido@pm.me>
2025-03-16 10:43:03 +0100

    Page publique pour présenter les pôles

---
+++ src/routes/poles/index.tsx
@@ -0,0 +1,87 @@
+import { component$ } from "@builder.io/qwik";
+import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
+import { LuLoader2 } from "@qwikest/icons/lucide";
+import { createCache } from "qwache";
+import type { RecordId } from "surrealdb";
+import db from "~/lib/db";
+
+import Pole, { type PoleProps } from "~/components/poles/Pole";
+
+const cache = createCache();
+
+export const QUERY = `SELECT 
+    id,
+    nom, 
+    meta.boutons as boutons, 
+    string::html::sanitize(meta.description) as description, 
+    meta.style as style, 
+    meta.images as images,
+    (
+        SELECT COUNT(heures) AS nb, math::mean(heures) AS heures 
+        FROM membres 
+        WHERE $parent.id INSIDE poles
+        GROUP ALL
+    ) AS membres
+FROM poles;`;
+
+export const usePoles = routeLoader$(async (requestEvent) => {
+    // This happens server-side
+    
+    // using 24h cache
+    return await cache('poles', async () => {
+        const client = await db();
+
+        // authentification
+        const USER = requestEvent.env.get('TIDE_USER');
+        const PASS = requestEvent.env.get('TIDE_PASS');
+        if(!USER || !PASS) {
+            console.error('⚠️ You didn\'t set env variables TIDE_USER & TIDE_PASS');
+            return []
+        }
+
+        await client.signin({
+            namespace: 'tidee',
+            database: 'data',
+            username: USER,
+            password: PASS
+        })
+
+        // Data fetching
+        const poles = await client.query<[(PoleProps & { id: RecordId })[]]>(QUERY)
+
+        return poles[0].map(pole => ({
+            ...pole,
+            id: pole.id.id.toString()
+        }))
+    }, 1000 * 60 * 60 * 24)
+})
+
+export default component$(() => {
+
+    const poles = usePoles();
+
+    if(!poles.value) {
+        return <section class="w-screen h-screen flex items-center justify-center">
+            <LuLoader2 class="animate-spin"/>
+
+        </section>
+    }
+
+    return <section class="h-screen w-full overflow-x-auto snap-x snap-mandatory flex flex-row">
+        {
+            poles.value.map(pole => <Pole
+                key={pole.nom} 
+                id={pole.id}
+                nom={pole.nom}
+                description={pole.description}
+                boutons={pole.boutons}
+                style={pole.style}
+                images={pole.images}
+                membres={pole.membres}/>)
+        }
+    </section>
+})
+
+export const head: DocumentHead = {
+    title: "Tidee - Pôles"
+};
